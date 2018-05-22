2815bce759a4da35d6091d4a1702c18bee1f4973
AnastasiiaBendiukova <37987645+AnastasiiaBendiukova@users.noreply.github.com>
2018-05-22 22:16:37 +0200

    ff

--- warm_cloud_simulator.py
+++ warm_cloud_simulator.py
@@ -76,29 +76,29 @@ r_mean = 0.000030531
 v_mean = 4 * np.pi/3 * r_mean**3
 r_sr = 30.531*10**(-6)
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 r_mean=0.000030531
 v_mean=4*np.pi/3*r_mean**3
 #<<<<<<< HEAD
 eta=np.repeat(V*N/n0,n0)
 #=======
-=======
->>>>>>> fb5a89bca6ce4d93303edc06d97d7d9f4d92217c
+#=======
+#>>>>>>> fb5a89bca6ce4d93303edc06d97d7d9f4d92217c
 
 eta = np.repeat(V * N/n0, n0) #funkcja krotnosci
 print(np.shape(eta))
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #>>>>>>> e166a11f9aa094fe72d47f5fbebd9c65968c4d20
-v=inverse_transform_sampling(f(np.arange(0,1,0.001),v_mean),100,N)
-print(v.shape)
+#v=inverse_transform_sampling(f(np.arange(0,1,0.001),v_mean),100,N)
+#print(v.shape)
 #v=f(np.random.uniform(1000),v_mean)
 #pl.hist(v)
-r=(3/(4*np.pi)*v)**(1/3)
-=======
+#r=(3/(4*np.pi)*v)**(1/3)
+#=======
 
 r = inverse_transform_sampling(N, r_sr) #wektor promieni
 #print(radii)
->>>>>>> fb5a89bca6ce4d93303edc06d97d7d9f4d92217c
+#>>>>>>> fb5a89bca6ce4d93303edc06d97d7d9f4d92217c
 pl.hist(r)
 pl.show()
 
@@ -137,20 +137,44 @@ for i in range(300):
     
 pl.plot(rad,vel,'ro')
 
+
+
 # TURBULENCJA - DEFINICJE
 
-energy_disipation = 0.5  # do ustalenia, m^2/s^3
-kolm_length = (nu_a**3/energy_disipation)**(1/4)
-r_d = 10**(-6)  # do ustalenia, length scale of the acceleration diffusion
-St = 0.5  # Stokes number, do ustalenia, odczytany dla <r> = 30 mikrom
-          # energy_disipation = 0.5
+en_dis = 0.01  # energy dissipation
+kolm_length = (nu_a**3/en_dis)**(1/4)
+kolm_time=(nu_a/en_dis)**(1/2)
+kolm_vel=(nu_a*en_dis)**(1/4)
+Rey=200 #Reynolds
+
+
+          
+
+def relax_time(k): #obliczenia czasu  relaksacji - zalezy od promienia czastki
+    return ro_w*ro_a*r[k]**2/(18*nu_a)
+
+def Stokes_num(j):
+    return relax_time(j)/kolm_time
+
+def C_1(i): #stala potrzebna do dalszych obliczen
+    f1=0.1886*np.exp(20.306/Rey)
+    f2=-0.1988*Stokes_num(i)**4 + 1.5275*Stokes_num(i)**3 - 4.2942*Stokes_num(i)**2 + 5.3406*Stokes_num(i)
+    return f2/(g*kolm_time/kolm_vel)**f1
 
 def g_12(j, k):
-    return ((kolm_length**2 + r_d**2)/((max(r[j], r[k]))**2 + r_d**2))**(c_1/2)
+    a_o=(11+7*Rey)/(205+Rey)
+    a_og=a_o+np.pi/8*(g**2*kolm_time/kolm_vel)**2
+    Fa=20.115*(a_og/Rey)**(1/2)
+    r_d=kolm_length*(abs(Stokes_num(j)-Stokes_num(k))*Fa)**(1/2)#length scale of the acceleration diffusion
+    return ((kolm_length**2 + r_d**2)/((max(r[j], r[k]))**2 + r_d**2))**(max(C_1(j),C_1(k))/2)
+
+def K(i,j): #koncowy czlon turbulencyjny
+    w=(200/(15/en_dis/nu_a)**(1/2))**(1/2)
+    return 2*np.pi*(r[i]+r[j])**2*w*g_12(i,j)
 
 
 
-
+#Czlon gravitacyjny
 
 def efficiency(j, k):
     #do rozbudowy, na razie stała
